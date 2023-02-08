fa19d5b4c2fd7f47913e162c6544fe1f4d8f8a1c
Katarzyna KLEPACKA <kklepacka@nx-domain.lan>
2023-02-08 15:58:13 +0100

    lol

--- src/test/java/testcscu4/StepDefinition.java
+++ src/test/java/testcscu4/StepDefinition.java
@@ -1,109 +1,75 @@
 package testcscu4;
 
-import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
 
 public class StepDefinition {
 
-    @When("test '")
-    public void test1(String docString) {
-
-        System.out.println("test");
+    @Given("test '")
+    public void test1(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test ` echo test `")
-    public void test2(String docString) {
-
-        System.out.println("test");
+    @Given("Test ` echo test `")
+    public void test2(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test $test")
-    public void test3(String docString) {
-
-        System.out.println("test");
+    @Given("Test $test")
+    public void test3(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test & echo toto")
-    public void test4(String docString) {
-
-        System.out.println("test");
+    @Given("Test & echo toto")
+    public void test4(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test ; echo toto")
-    public void test5(String docString) {
-
-        System.out.println("test");
+    @Given("Test ; echo toto")
+    public void test5(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test | echo toto")
-    public void test6(String docString) {
-
-        System.out.println("test");
+    @Given("Test | echo toto")
+    public void test6(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test ^")
-    public void test7(String docString) {
-
-        System.out.println("test");
+    @Given("Test ^")
+    public void test7(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test %test%")
-    public void test8(String docString) {
-
-        System.out.println("test");
+    @Given("Test %test%")
+    public void test8(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test $\\{echo test}")
-    public void test9(String docString) {
-
-        System.out.println("test");
+    @Given("Test $\\{echo test}")
+    public void test9(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test $\\(echo test)")
-    public void test10(String docString) {
-
-        System.out.println("test");
+    @Given("Test $\\(echo test)")
+    public void test10(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test #test")
-    public void test11(String docString) {
-
-        System.out.println("test");
+    @Given("Test #test")
+    public void test11(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test 𨱏")
-    public void test12(String docString) {
-
-        System.out.println("test");
+    @Given("Test 𨱏")
+    public void test12(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test 👩‍❤️‍💋‍👨")
-    public void test13(String docString) {
-
-        System.out.println("test");
+    @Given("Test 👩‍❤️‍💋‍👨")
+    public void test13(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test [test]")
-    public void test14(String docString) {
-
-        System.out.println("test");
+    @Given("Test [test]")
+    public void test14(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test ^^")
-    public void test15(String docString) {
-
-        System.out.println("test");
+    @Given("Test ^^")
+    public void test15(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test ,.+-_~@€çé=àû*?!")
-    public void test16(String docString) {
-
-        System.out.println("test");
+    @Given("Test ,.+-_~@?çé=àû*?!")
+    public void test16(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
-
-    @When("Test .\\/test")
-    public void test17(String docString) {
-
-        System.out.println("test");
-
+    @Given("Test .\\/test")
+    public void test17(io.cucumber.datatable.DataTable dataTable) {
+            System.out.println("test");
     }
 }
